1cf1cd14a1ae2d2b5a995bf37168f2c2ae70905a
Jonas Denardi <jonas_denardi@hotmail.com>
2026-05-03 18:33:00 -0300

    Adjusts formatting

--- example-02-winning-any-game/machine-learning/worker.js
+++ example-02-winning-any-game/machine-learning/worker.js
@@ -2,23 +2,24 @@ importScripts('https://cdn.jsdelivr.net/npm/@tensorflow/tfjs@latest');
 
 const MODEL_PATH = `yolov5n_web_model/model.json`;
 const LABELS_PATH = `yolov5n_web_model/labels.json`;
-const INPUT_MODEL_DIMENTIONS = 640;
-const CLASS_THRESHOLD = 0.5;
+const INPUT_MODEL_DIMENTIONS = 640
+const CLASS_THRESHOLD = 0.4
 
 let _labels = []
-let _model = []
-
+let _model = null
 async function loadModelAndLabels() {
-    await tf.ready();
-    _labels = await (await fetch(LABELS_PATH)).json();
-    _model = await tf.loadGraphModel(MODEL_PATH);
+    await tf.ready()
+
+    _labels = await (await fetch(LABELS_PATH)).json()
+    _model = await tf.loadGraphModel(MODEL_PATH)
 
     // warmup
-    const dummyInput = tf.ones(_model.inputs[0].shape);
-    await _model.executeAsync(dummyInput);
-    tf.dispose(dummyInput);
+    const dummyInput = tf.ones(_model.inputs[0].shape)
+    await _model.executeAsync(dummyInput)
+    tf.dispose(dummyInput)
 
-    postMessage({ type: 'model-loaded' });
+    postMessage({ type: 'model-loaded' })
+
 }
 
 /**
@@ -34,37 +35,36 @@ async function loadModelAndLabels() {
  */
 function preprocessImage(input) {
     return tf.tidy(() => {
-        const image = tf.browser.fromPixels(input);
+        const image = tf.browser.fromPixels(input)
 
         return tf.image
             .resizeBilinear(image, [INPUT_MODEL_DIMENTIONS, INPUT_MODEL_DIMENTIONS])
             .div(255)
-            .expandDims(0);
+            .expandDims(0)
     })
 }
 
 async function runInference(tensor) {
-    const output = await _model.executeAsync(tensor);
-    tf.dispose(tensor);
-
+    const output = await _model.executeAsync(tensor)
+    tf.dispose(tensor)
     // Assume que as 3 primeiras saídas são:
     // caixas (boxes), pontuações (scores) e classes
-    const [boxes, scores, classes] = output.slice(0 , 3)
 
-    const [boxsData, scoresData, classesData] = await Promise.all(
+    const [boxes, scores, classes] = output.slice(0, 3)
+    const [boxesData, scoresData, classesData] = await Promise.all(
         [
             boxes.data(),
             scores.data(),
             classes.data(),
         ]
-    );
+    )
 
-    output.forEach(t => t.dispose());
+    output.forEach(t => t.dispose())
 
     return {
-        boxes: boxsData,
+        boxes: boxesData,
         scores: scoresData,
-        classes: classesData,
+        classes: classesData
     }
 }
 
@@ -80,28 +80,28 @@ async function runInference(tensor) {
  */
 function* processPrediction({ boxes, scores, classes }, width, height) {
     for (let index = 0; index < scores.length; index++) {
-        if(scores[index] < CLASS_THRESHOLD) continue
+        if (scores[index] < CLASS_THRESHOLD) continue
 
-        const label = _labels[classes[index]];
-        if(label !== 'kite') continue
+        const label = _labels[classes[index]]
+        if (label !== 'kite') continue
 
         let [x1, y1, x2, y2] = boxes.slice(index * 4, (index + 1) * 4)
-
         x1 *= width
         x2 *= width
         y1 *= height
         y2 *= height
 
-        const boxWith = x2 - x1
+        const boxWidth = x2 - x1
         const boxHeight = y2 - y1
-        const centerX = x1 + boxWith / 2
+        const centerX = x1 + boxWidth / 2
         const centerY = y1 + boxHeight / 2
 
-        yield{
+        yield {
             x: centerX,
             y: centerY,
             score: (scores[index] * 100).toFixed(2)
-        }        
+        }
+
     }
 }
 
@@ -109,18 +109,18 @@ loadModelAndLabels()
 
 self.onmessage = async ({ data }) => {
     if (data.type !== 'predict') return
-    if(!_model) return
+    if (!_model) return
 
-    const input = preprocessImage(data.image);
-    const { width, height } = data.image;
+    const input = preprocessImage(data.image)
+    const { width, height } = data.image
 
-    const inferenceResults = await runInference(input);
+    const inferenceResults = await runInference(input)
 
-    for (const prediction of processPrediction(inferenceResults, width, height)){
+    for (const prediction of processPrediction(inferenceResults, width, height)) {
         postMessage({
-        type: 'prediction',
-        ...prediction
-    });
+            type: 'prediction',
+            ...prediction
+        });
     }
 };
 
